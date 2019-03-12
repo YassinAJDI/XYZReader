14e846946ed4a875046a042a1efe5e50f8db6523
Yassin AJDI <yassin.ajdi@gmail.com>
2019-03-12 17:46:46 +0100

    Update json data url which improve performance

--- XYZReader/src/main/java/com/example/xyzreader/remote/Config.java
+++ XYZReader/src/main/java/com/example/xyzreader/remote/Config.java
@@ -12,7 +12,7 @@ public class Config {
     static {
         URL url = null;
         try {
-            url = new URL("https://go.udacity.com/xyz-reader-json" );
+            url = new URL("https://nspf.github.io/XYZReader/data.json");
         } catch (MalformedURLException ignored) {
             // TODO: throw a real error
             Log.e(TAG, "Please check your internet connection.");
