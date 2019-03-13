acae638cf7e47f29fe7a9893e52313a7de3f5a8c
Yassin AJDI <yassin.ajdi@gmail.com>
2019-03-13 07:43:19 +0100

    Use url with correct ratio

--- XYZReader/src/main/java/com/example/xyzreader/remote/Config.java
+++ XYZReader/src/main/java/com/example/xyzreader/remote/Config.java
@@ -12,7 +12,7 @@ public class Config {
     static {
         URL url = null;
         try {
-            url = new URL("https://nspf.github.io/XYZReader/data.json");
+            url = new URL("https://raw.githubusercontent.com/SuperAwesomeness/XYZReader/master/data.json");
         } catch (MalformedURLException ignored) {
             // TODO: throw a real error
             Log.e(TAG, "Please check your internet connection.");
